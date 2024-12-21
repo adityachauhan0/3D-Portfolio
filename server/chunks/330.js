8e5d2509806ff2d450a6b67f143ed446cc4aeda2
adityachauhan0 <adityac.1406@gmail.com>
2024-12-21 10:34:27 +0000

    Updates

--- server/chunks/330.js
+++
@@ -1,1 +0,0 @@
-"use strict";exports.id=330,exports.ids=[330],exports.modules={8330:(e,t,o)=>{o.r(t),o.d(t,{default:()=>r});let r=(0,o(8570).createProxy)(String.raw`/home/momotaaro/Next.js-Creative-Portfolio-Website/src/components/models/Wizard.jsx#default`)}};
--- server/chunks/672.js
+++
@@ -1,1 +0,0 @@
-"use strict";exports.id=672,exports.ids=[672],exports.modules={7672:(e,t,o)=>{o.r(t),o.d(t,{default:()=>r});let r=(0,o(8570).createProxy)(String.raw`/home/momotaaro/Next.js-Creative-Portfolio-Website/src/components/models/Staff.jsx#default`)}};

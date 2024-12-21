877bef6e3747af2448b83b956c3847ad70c274d1
adityachauhan0 <adityac.1406@gmail.com>
2024-12-21 10:03:46 +0000

    Initial commit

---
+++ src/app/data.js
@@ -0,0 +1,81 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Movie Recommender ML Model",
+    description: "SKLearn Python StreamLit",
+    date: "2022-08-15",
+    demoLink: "https://movie-suggestor.streamlit.app/",
+  },
+  {
+    id: 2,
+    name: "Meditation App",
+    description: "React Native, Expo Router, Native Wind, JScript, TypeScript",
+    date: "2023-06-20",
+    demoLink: "https://github.com/adityachauhan0/meditation-app",
+  },
+  {
+    id: 3,
+    name: "This Website!!",
+    description: "ThreeJS, NextJS, Taiwind CSS, Framer Motion, React Hook Form, Sonner, Gltf JSX",
+    date: "2024-09-10",
+    demoLink: "https://portfol-adityac.streamlit.app/",
+  },
+  {
+    id: 4,
+    name: "TCP FileStorage",
+    description: "Decentralized File Storage System using GoLang",
+    date: "2023-10-30",
+    demoLink: "https://github.com/adityachauhan0/TCP-FileStorage",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/adityachauhan0",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/adityamc/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "X",
+    link: "https://adityachauhan0.github.io/portfolio/",
+    icon: "twitter",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "https://github.com/adityachauhan0/ResumePDF/blob/main/Resume_Main.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
---
+++ src/components/about/index.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import ItemLayout from "./ItemLayout";
+import Link from "next/link";
+
+const AboutDetails = () => {
+  return (
+    <section className="py-20 w-full">
+      <div className="grid grid-cols-12 gap-4 xs:gap-6  md:gap-8 w-full">
+        <ItemLayout
+          className={
+            " col-span-full lg:col-span-8 row-span-2 flex-col items-start"
+          }
+        >
+          <h2 className="  text-xl md:text-2xl text-left w-full capitalize">
+            Painter Of This Piece Of Art
+          </h2>
+          <p className="font-light  text-xs sm:text-sm md:text-base   ">
+          I am Aditya Chauhan, a passionate developer currently exploring the realms of Web and App Development, Machine Learning, and Competitive Programming. As a B.Tech student at the prestigious Indraprastha Institute of Information Technology, Delhi, I am continually refining my skills in various domains like Data Structures, Algorithms, Electronics, and VLSI Engineering.
+
+My toolkit includes an array of languages and frameworks, from C++ and Java to React.js, Node.js, and Kotlin, allowing me to build scalable and efficient systems. Whether it’s crafting dynamic, user-friendly websites with React.js, creating cross-platform mobile apps with React Native, or diving deep into the world of machine learning with TensorFlow and Keras, I aim to blend technical precision with creativity.
+
+I thrive on challenges and take pride in tackling complex problems—be it in the form of algorithmic puzzles or in developing systems that span from decentralized file storage solutions to advanced image segmentation models. With an ever-growing curiosity and a commitment to learning, I am excited to shape the future of technology while continuously honing my craft.
+
+Let’s connect and explore how we can collaborate on the next groundbreaking project!
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={" col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            25+ <sub className="font-semibold text-base">clients</sub>
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={"col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            4+{" "}
+            <sub className="font-semibold text-base">years of experience</sub>
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={"col-span-full sm:col-span-6 md:col-span-4 !p-0"}
+        >
+          <img
+            className="w-full h-auto"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/top-langs?username=codebucks27&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            alt="CodeBucks"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-8 !p-0"}>
+          <img
+            className="w-full h-auto"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api?username=codebucks27&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            alt="CodeBucks"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full"}>
+          <img
+            className="w-full h-auto"
+            src={`https://skillicons.dev/icons?i=appwrite,aws,babel,bootstrap,cloudflare,css,d3,docker,figma,firebase,gatsby,git,github,graphql,html,ipfs,js,jquery,kubernetes,linux,mongodb,mysql,netlify,nextjs,nodejs,npm,postgres,react,redux,replit,sass,supabase,tailwind,threejs,vercel,vite,vscode,yarn`}
+            alt="CodeBucks"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+          <img
+            className="w-full h-auto"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STREAK_STATS_URL}?user=codebucks27&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B`}
+            alt="CodeBucks"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+          <Link
+            href="https://github.com/codebucks27/Nextjs-contentlayer-blog"
+            target="_blank"
+            className="w-full"
+          >
+            <img
+              className="w-full h-auto"
+              src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/pin/?username=codebucks27&repo=Nextjs-contentlayer-blog&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&description_lines_count=2`}
+              alt="CodeBucks"
+              loading="lazy"
+            />
+          </Link>
+        </ItemLayout>
+      </div>
+    </section>
+  );
+};
+
+export default AboutDetails;
